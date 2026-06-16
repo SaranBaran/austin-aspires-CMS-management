8f3bae35e32c77dcef3ab8c0a4cea78491723551
SaranBaran <kilicsaryabaran@gmail.com>
2026-06-16 01:13:36 -0400

    search bar

--- src/pages/CMS Design (List).bzy4n.js
+++ src/pages/CMS Design (List).bzy4n.js
@@ -1,11 +1,27 @@
 import wixData from 'wix-data';
 
+let debounceTimer;
+
 $w.onReady(function () {
+
     $w('#input1').onInput(() => {
-        let searchValue = $w('#input1').value;
         
-        $w('#dataset1').setFilter(wixData.filter()
-            .contains('cmsFieldName', searchValue) 
-        );
+        // clear timer
+        clearTimeout(debounceTimer);
+        
+        // wait time after text
+        debounceTimer = setTimeout(() => {
+            let searchValue = $w('#input1').value;
+            
+            if (searchValue && searchValue.trim() !== "") {
+                // Filter 
+                $w('#dynamicDataset').setFilter(wixData.filter()
+                    .contains('resourceName', searchValue)
+                );
+            } else {
+                // empty show everything
+                $w('#dynamicDataset').setFilter(wixData.filter());
+            }
+        }, 200); 
     });
 });
